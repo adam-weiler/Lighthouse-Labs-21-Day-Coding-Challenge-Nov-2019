e4d0874f0768906719c0e034de83515d14e29f6b
adam-weiler <adam.weiler@yahoo.ca>
2019-11-01 16:17:33 -0400

    Adding solution for Day 1.

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const doorToDoor = (volunteers, neighbourhoods) => {
-  // Code here!
-  // Remember to return a value
-  // Want to see what's going on inside your function? You can use console.log() to debug and print variables to the console below.
-};
+// My Functions:
+// Day #1: Return the number of neighbourhoods each volunteer should visit to split the work evenly.
+const doorToDoor = (volunteers, neighbourhoods) =>
+  neighbourhoods.length / volunteers.length;
+
+// Inputs provided:
+// Day #1
+const volunteers = ['Sally', 'Jake', 'Brian', 'Hamid'];
+
+// Day #1
+const neighbourhoods = [
+  'Central Valley',
+  'Big Mountain',
+  'Little Bridge',
+  'Bricktown',
+  'Brownsville',
+  "Paul's Boutique",
+  'Clay Park',
+  'Fox Nest'
+];
+
+// Calling functions:
+// Day #1
+doorToDoor(volunteers, neighbourhoods);
