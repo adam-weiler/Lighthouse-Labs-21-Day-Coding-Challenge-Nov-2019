3a8569e152cb82239f0495bd26997ea3f3c08ba3
adam-weiler <adam.weiler@yahoo.ca>
2019-11-04 16:00:42 -0500

    Adding solution for Day 4.

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ const interviewAnswer = topic => {
 };
 
 // Day #3: Return an array of updated votes based on the name of the selected candidate.
-const castVote = (name, votes) => {
-  switch (name) {
+const castVote = (candidate, votes) => {
+  switch (candidate) {
     case 'Tim': {
       votes[0]++;
       break;
@@ -40,15 +40,14 @@ const castVote = (name, votes) => {
   return votes;
 };
 
-// Day #4: Return an array of updated unregisteredVoters by removing the name of the person voting.
-const registerToVote = (name, unregisteredVoters) => {
-  // Code here!
-  // Remember to return a value!
-};
+// Day #4: Return an array of updated unregisteredVoters by removing the voter.
+const registerToVote = (voter, unregisteredVoters) =>
+  unregisteredVoters.filter(n => n !== voter);
 
 // Inputs provided:
 // Day #1
 const volunteers = ['Sally', 'Jake', 'Brian', 'Hamid'];
+const volunteers2 = ['Sally', 'Jake', 'Brian'];
 
 const neighbourhoods = [
   'Central Valley',
@@ -60,6 +59,7 @@ const neighbourhoods = [
   'Clay Park',
   'Fox Nest'
 ];
+const neighbourhoods2 = ['Central Valley', 'Big Mountain', 'Little Bridge'];
 
 // Day #2
 const input = 'arts funding';
@@ -68,25 +68,40 @@ const input3 = 'transportation';
 const input4 = 'infidelity';
 
 // Day 3
-const name = 'Sally';
-const name2 = 'Tim';
-const name3 = 'Beth';
+const candidate = 'Sally';
+const candidate2 = 'Tim';
+const candidate3 = 'Beth';
 
 const votes = [0, 2, 1];
 const votes2 = [1, 1, 2];
 const votes3 = [5, 2, 2];
 
+// Day 4
+const voter = 'Bradley';
+const voter2 = 'Bobby';
+const voter3 = 'Floyd';
+
+const unregisteredVoters = ['Jake', 'Alanna', 'Bradley', 'Stephanie'];
+const unregisteredVoters2 = ['Jimmy', 'Suzie', 'Bobby'];
+const unregisteredVoters3 = ['Tomasa', 'Tennille', 'Tayna', 'Eufemia', 'Floyd'];
+
 // Calling functions:
 // Day #1
-doorToDoor(volunteers, neighbourhoods);
+doorToDoor(volunteers, neighbourhoods); // 2
+doorToDoor(volunteers2, neighbourhoods2); // 1
 
 // Day #2
-interviewAnswer(input);
-interviewAnswer(input2);
-interviewAnswer(input3);
-interviewAnswer(input4);
+interviewAnswer(input); // We'll have to get creative!
+interviewAnswer(input2); // Time is money.
+interviewAnswer(input3); // It's going to be a long road, so we better get moving.
+interviewAnswer(input4); // QUACK
 
 // Day #3
-castVote(name, [0, 2, 1]);
-castVote(name2, [1, 1, 2]);
-castVote(name3, [5, 2, 2]);
+castVote(candidate, [0, 2, 1]); // [0, 3, 1]
+castVote(candidate2, [1, 1, 2]); // [2, 1, 2]
+castVote(candidate3, [5, 2, 2]); // [5, 2, 3]
+
+// Day #4
+registerToVote(voter, unregisteredVoters); // ['Jake', 'Alanna', 'Stephanie']
+registerToVote(voter2, unregisteredVoters2); // ['Jimmy', 'Suzie']
+registerToVote(voter3, unregisteredVoters3); // ['Tomasa', 'Tennille', 'Tayna', 'Eufemia']
